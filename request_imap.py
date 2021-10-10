ee95a5b0dba459cf29b493a7842be066c9f3616f
yashkins <yashkins@yandex.ru>
2021-10-10 21:49:25 +1000

    разобрался с ошибкой идент-ции. Добавлена возможность получать имя отправителя и список uid. При попытке получить все имена отправителей программа очень долго это обрабытывает.

--- request_imap.py
+++ request_imap.py
@@ -1,9 +1,19 @@
 import imaplib
-import getpass
+import email
+import seting
 
+password = seting.password
 mail = imaplib.IMAP4_SSL('imap.yandex.ru')
-mail.login('yashkins@yandex.ru', getpass.getpass())
+mail.login('yashkins@yandex.ru', password)
 mail.select('inbox')
 result, data = mail.uid('search', None, 'ALL')
 list_uid = data[0].split()
-print(list_uid)
+list_name = []
+i = list_uid[0]
+result, data = mail.uid('fetch', i, '(RFC822)')
+raw_email = data[0][1]
+email_message = email.message_from_bytes(raw_email)
+name = email.utils.parseaddr(email_message['From'])
+list_name.append(name)
+print(name[0])
+
