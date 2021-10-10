052f4ffc538e25bfe8051736cf2c2d816c346879
yashkins <yashkins@yandex.ru>
2021-10-10 23:50:27 +1000

    представил код в виде фукции get_imap, которая возвращает словарь имен отправителей и uid писем, которые им пренадлежат

--- request_imap.py
+++ request_imap.py
@@ -1,19 +1,29 @@
+import base64
 import imaplib
 import email
-import seting
+import settings
 
-password = seting.password
-mail = imaplib.IMAP4_SSL('imap.yandex.ru')
-mail.login('yashkins@yandex.ru', password)
-mail.select('inbox')
-result, data = mail.uid('search', None, 'ALL')
-list_uid = data[0].split()
-list_name = []
-i = list_uid[0]
-result, data = mail.uid('fetch', i, '(RFC822)')
-raw_email = data[0][1]
-email_message = email.message_from_bytes(raw_email)
-name = email.utils.parseaddr(email_message['From'])
-list_name.append(name)
-print(name[0])
+
+def get_imap(login=None, password=None, date=None):
+    password = settings.password
+    login = settings.login
+    mail = imaplib.IMAP4_SSL('imap.yandex.ru')
+    mail.login(login, password)
+    mail.select('inbox')
+    if not date:
+        result, data = mail.uid('search', None, 'ALL')
+    else: 
+        result, data = mail.uid('search', None, '(SENTSINCE{date})'.format(date=date))
+    list_uid = data[0].split()
+    dict_name_uid = {}
+    for id in list_uid:
+        result, data = mail.uid('fetch', id, '(RFC822)')
+        raw_email = data[0][1]
+        email_message = email.message_from_bytes(raw_email)
+        name = email.utils.parseaddr(email_message['From'])
+        name = name[0]
+        dict_name_uid = dict_name_uid[name].get(name, []).append(id)
+    return dict_name_uid
+
+    
 
