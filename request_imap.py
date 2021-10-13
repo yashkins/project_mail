c2c4fe324a04f543e9239592898dd48d6a4a744e
yashkins <yashkins@yandex.ru>
2021-10-13 23:54:40 +1000

    разбил фун-цию get_imap на две фун-ции

--- request_imap.py
+++ request_imap.py
@@ -14,16 +14,25 @@ def get_imap(login=None, password=None, date=None):
         result, data = mail.uid('search', None, 'ALL')
     else: 
         result, data = mail.uid('search', None, '(SENTSINCE{date})'.format(date=date))
-    list_uid = data[0].split()
+    list_uids = data[0].split()
+    return list_uids, mail
+
+
+def create_dict_name_uid():    
     dict_name_uid = {}
-    for id in list_uid:
+    list_uids, mail = get_imap()
+    for id in list_uids:
         result, data = mail.uid('fetch', id, '(RFC822)')
         raw_email = data[0][1]
         email_message = email.message_from_bytes(raw_email)
         name = email.utils.parseaddr(email_message['From'])
-        name = name[0]
-        dict_name_uid = dict_name_uid[name].get(name, []).append(id)
+        name = base64.b64decode(name[0][:-1].encode()).decode(errors='ignore')
+        dict_name_uid[name] = dict_name_uid.get(name, set()).add(id)
+        break
     return dict_name_uid
+
+for k,v in create_dict_name_uid().items():
+    print(k,v)
 
     
 
