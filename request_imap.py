6c34e4a47d174b22272cda2c156183873e8550a8
yashkins <yashkins@yandex.ru>
2021-10-26 21:49:01 +1000

    внес изменения в ф-цию create_dict_name_uidона получает только заголовки писем, а не все тело, и не по одному письму, а по 500. Исправленны мелкие ошибки. Все ф-ции работают.

--- request_imap.py
+++ request_imap.py
@@ -1,7 +1,9 @@
 import base64
+import datetime 
 import imaplib
 import email
 import settings
+from tqdm import tqdm
 
 
 def get_imap(login=None, password=None, date=None):
@@ -9,11 +11,11 @@ def get_imap(login=None, password=None, date=None):
     login = settings.login
     mail = imaplib.IMAP4_SSL('imap.yandex.ru')
     mail.login(login, password)
-    mail.select('inbox')
+    mail.select('Inbox')
     if not date:
         result, data = mail.uid('search', None, 'ALL')
     else: 
-        result, data = mail.uid('search', None, '(SENTSINCE{date})'.format(date=date))
+        result, data = mail.uid('search', None, '(SINCE {date})'.format(date=date))
     list_uids = data[0].split()
     return list_uids, mail
 
@@ -21,22 +23,36 @@ def get_imap(login=None, password=None, date=None):
 def create_dict_name_uid():    
     dict_name_uid = {}
     list_uids, mail = get_imap()
-    for id in list_uids:
-        result, data = mail.uid('fetch', id, '(RFC822)')
-        raw_email = data[0][1]
-        email_message = email.message_from_bytes(raw_email)
-        name, adress = email.utils.parseaddr(email_message['From'])
-        """name = base64.b64decode(name.encode()).decode(errors='ignore') если разберусь с кодировкой, то добавлю эту строку"""
-        dict_name_uid[adress] = dict_name_uid.get(adress, []) + [id]
-        
+    if not list_uids:
+        return dict_name_uid
+    for i in range(0,len(list_uids),500):
+        uids = list_uids[i:i+500]  
+        result, data = mail.uid('fetch', b','.join(uids), '(RFC822.HEADER)')
+        for key, elem in enumerate(data[::2]):
+            raw_email = elem[1]
+            email_message = email.message_from_bytes(raw_email)
+            name, adress = email.utils.parseaddr(email_message['From'])
+            """name = base64.b64decode(name.encode()).decode(errors='ignore') если разберусь с кодировкой, то добавлю эту строку"""
+            dict_name_uid[adress] = dict_name_uid.get(adress, []) + [uids[key]]  
+        break    
     return dict_name_uid
 
 
 def delete(list_uids, mail):
     for uid in list_uids:
-        mail.store(uid, '+FLAGS', '\\Deleted')
-        mail.expunge()
+        mail.uid('STORE', uid, '+FLAGS', '(\\Deleted)')
+    mail.expunge()
         
 
+#date_now = datetime.date.today().strftime("%d-%b-%Y")
+#print(date_now)
+  
 
-
+dict_name = create_dict_name_uid()
+for k,v in dict_name.items():
+    print(k,len(v))
+"""
+list_uids, mail = get_imap(date=date_now)
+print(list_uids)
+#delete(list_uids[-1:-2:-1], mail)
+"""
