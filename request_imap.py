e233a5b74ad4694a27c65dce631094b64747ca1b
yashkins <89984078+yashkins@users.noreply.github.com>
2021-10-27 21:48:44 +1000

    Feature/create request imap (#2)
    * создан сервер и шаблон главной страницы(заготовка)
    
    * создана функция подключения к п.ящику. Выдает ошибку идентификации
    
    * разобрался с ошибкой идент-ции. Добавлена возможность получать имя отправителя и список uid. При попытке получить все имена отправителей программа очень долго это обрабытывает.
    
    * представил код в виде фукции get_imap, которая возвращает словарь имен отправителей и uid писем, которые им пренадлежат
    
    * разбил фун-цию get_imap на две фун-ции
    
    * добавил функцию удаления писем из Почт. ящика
    
    * внес изменения в ф-цию create_dict_name_uidона получает только заголовки писем, а не все тело, и не по одному письму, а по 500. Исправленны мелкие ошибки. Все ф-ции работают.
    
    * передал ф-ции create_dict_name_uid в качестве параметров list_uids и mail

---
+++ request_imap.py
@@ -0,0 +1,44 @@
+import base64
+import datetime 
+import imaplib
+import email
+import settings
+from tqdm import tqdm
+
+
+def get_imap(login=None, password=None, date=None):
+    password = settings.password
+    login = settings.login
+    mail = imaplib.IMAP4_SSL('imap.yandex.ru')
+    mail.login(login, password)
+    mail.select('Inbox')
+    if not date:
+        result, data = mail.uid('search', None, 'ALL')
+    else: 
+        result, data = mail.uid('search', None, '(SINCE {date})'.format(date=date))
+    list_uids = data[0].split()
+    return list_uids, mail
+
+
+def create_dict_name_uid(list_uids, mail):    
+    dict_name_uid = {}
+    if not list_uids:
+        return dict_name_uid
+    for i in range(0,len(list_uids),500):
+        uids = list_uids[i:i+500]  
+        result, data = mail.uid('fetch', b','.join(uids), '(RFC822.HEADER)')
+        for key, elem in enumerate(data[::2]):
+            raw_email = elem[1]
+            email_message = email.message_from_bytes(raw_email)
+            name, adress = email.utils.parseaddr(email_message['From'])
+            """name = base64.b64decode(name.encode()).decode(errors='ignore') если разберусь с кодировкой, то добавлю эту строку"""
+            dict_name_uid[adress] = dict_name_uid.get(adress, []) + [uids[key]]  
+    return dict_name_uid
+
+
+def delete(list_uids, mail):
+    for uid in list_uids:
+        mail.uid('STORE', uid, '+FLAGS', '(\\Deleted)')
+    mail.expunge()
+        
+
