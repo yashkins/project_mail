1134e2378e07ddf9d8a6a0f39d1a76534afdf0d9
yashkins <89984078+yashkins@users.noreply.github.com>
2021-11-08 23:07:25 +1000

    Feature/work whit thread (#4)
    * Переместил request_imap в папку webapp. В __init__ добавил вызов 2-х ф-ций и передал в render_template словарь dict_name_uid. Внес небольшие изменения в index.html
    
    * пока ничего не поменял
    
    * добавил в __init__ переменную list_name_count, которую передаю в шаблон. Начал редактировать шаблон
    
    * вроде бы ничего не поменял. Все работает:)
    
    * Внесенно распараллеливание потоков в ф-цию create_dict_name_uid. возникает ошибка при попытке обработать более 1499 писем.
    
    * Добавил в ф-цию create_dict_name_uid сортировку словаря по количеству писем, данная ф-ция теперь возвращает 2 словаря. Добавил блок try except в ф-цию parser. Скорректировал под изменненую ф-цию create_dict_name_uid модули __init__ и index

---
+++ webapp/request_imap.py
@@ -0,0 +1,77 @@
+import base64
+import datetime
+import imaplib
+import email
+import threading
+from collections import deque
+from webapp import settings
+import time
+
+
+def get_imap(login=None, password=None, date=None):
+    password = settings.password
+    login = settings.login
+    mail = imaplib.IMAP4_SSL('imap.yandex.ru')
+    mail.login(login, password)
+    mail.select('Inbox')
+    if not date:
+        result, data = mail.uid('search', None, 'ALL')
+    else:
+        result, data = mail.uid('search', None, '(SINCE {date})'.format(date=date))
+    list_uids = data[0].split()
+    return list_uids, mail
+
+def parser(outque, data, uids, i):
+    raw_email = data[i][1]
+    email_message = email.message_from_bytes(raw_email)
+    try:
+        name, adress = email.utils.parseaddr(email_message['From'])
+    except TypeError:
+        print("empty header", "Номер письма: ", uids[i])
+        return
+    outque.append((adress,uids[i]))
+
+def create_dict_name_uid(list_uids, mail):
+    dict_name_uid = {}
+    if not list_uids:
+        return dict_name_uid
+    for i in range(0,100,20):
+        uids = list_uids[i:i+20]
+        result, data = mail.uid('fetch', b','.join(uids), '(RFC822.HEADER)')
+        data = data[::2]
+        outque = deque()
+        list_join = []
+        for i in range(len(data)):
+            th = threading.Thread(target=parser,args=(outque, data, uids, i,))
+            list_join.append(th)
+            th.start()
+        for i in list_join:
+            i.join()
+        while True:
+            try:
+                adress, uid = outque.popleft()
+            except IndexError:
+                break
+            dict_name_uid[adress] = dict_name_uid.get(adress, []) + [uid]
+    dict_name_len = {k:len(v) for k,v in dict_name_uid.items()}
+    tuple_sort = sorted(dict_name_len.items(), key=lambda x:x[1],reverse=True)
+    dict_name_len = dict(tuple_sort)
+    return dict_name_uid, dict_name_len
+    
+
+
+def delete(list_uids, mail):
+    for uid in list_uids:
+        mail.uid('STORE', uid, '+FLAGS', '(\\Deleted)')
+    mail.expunge()
+
+
+if __name__ == "__main__":
+    mail, list_uids = get_imap()
+    start = time.time()
+    dict_name_uids = create_dict_name_uid(mail, list_uids)
+    end = time.time()
+    for k,v in dict_name_uids.items():
+        print(k,len(v))
+    print(sum([len(v) for k,v in dict_name_uids.items()]))
+    print(end-start)
